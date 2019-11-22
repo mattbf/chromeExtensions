5cfb6ea153b4b91dace67475e3ba2135bf63fbdf
Matthew Behan-Fossey <matthewbf8@gmail.com>
2019-11-21 18:18:31 -0800

    Added js interactivity

---
+++ BudgetManager/popup.js
@@ -0,0 +1,23 @@
+$(function(){
+  //When user click on spend amount button
+  $('#spendAmount').click(function(){
+    //if a total exisits add this value to the total
+    chrome.storage.sync.get('total', function(budget){
+      var newTotal = 0;
+      if (budget.total){
+        newTotal += parseInt(budget.total);
+      }
+
+      var amount = $('#amount').val();
+      if(amount){
+        newTotal += parseInt(amount)
+      }
+
+      // Update the chrome storage 
+      chrome.storage.sync.set({'total': newTotal})
+
+      $('#total').text(newTotal)
+      $('#amount').val('');
+    })
+  })
+})
